a06ef525b223ed7993df4dcf1ffd9be63d50ddf8
Tatyana <an-tatys@mail.ru>
2018-04-13 20:01:00 +0300

    add dotenv

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,20 +1,15 @@
 import firebase from 'firebase'
-// import firebaseui from 'firebaseui'
-// import 'firebaseui/dist/firebaseui.css'
-// import store from './store'
-// import * as firebase from 'firebase'
+
+debugger
 
 firebase.initializeApp({
-  apiKey: 'AIzaSyAxnhsCNaFB9OkwxPNTgjqTpNVumy_4Wyo',
-  authDomain: 'book-9601a.firebaseapp.com',
-  databaseURL: 'https://book-9601a.firebaseio.com',
-  projectId: 'book-9601a',
-  storageBucket: 'book-9601a.appspot.com',
-  messagingSenderId: '527252966937'
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID
 })
-// var db = appFirebase.database()
-// var booksRef = db.ref()
-// export default booksRef
 
 export default {
   database: firebase.database()
